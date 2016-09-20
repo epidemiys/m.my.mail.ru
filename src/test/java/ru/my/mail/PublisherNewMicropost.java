cfff12ac6cd78a3f0f42319a51e0a809f8aceaed
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-20 14:50:13 +0300

    Логин в систему
    Перенес процесс логина в @BeforeMetod

--- src/test/java/ru/my/mail/PublisherNewMicropost.java
+++ src/test/java/ru/my/mail/PublisherNewMicropost.java
@@ -1,3 +1,5 @@
+package ru.my.mail;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -13,10 +15,6 @@ public class PublisherNewMicropost {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void PublisherNewMicropost() {
         wd.get("http://m.my.mail.ru/cgi-bin/login");
         wd.findElement(By.name("Login")).click();
         wd.findElement(By.name("Login")).sendKeys("\\undefined");
@@ -29,6 +27,10 @@ public class PublisherNewMicropost {
         wd.findElement(By.name("Password")).clear();
         wd.findElement(By.name("Password")).sendKeys("Mytesting");
         wd.findElement(By.xpath("//div[@class='heightContent']/form/div[3]/input")).click();
+    }
+    
+    @Test
+    public void testPublisherNewMicropost() {
         wd.findElement(By.cssSelector("a.menu__item.m-publish")).click();
         wd.findElement(By.name("arg_text")).click();
         wd.findElement(By.name("arg_text")).clear();
