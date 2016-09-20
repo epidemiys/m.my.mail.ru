7533772bbda19dc3ad0bed9374c6f2b5eeadc7d4
epidemiys <aleksandr.petrov@aleksandrpetrov.local>
2016-09-20 15:14:39 +0300

    Рефакторинг

--- src/test/java/ru/my/mail/PublisherNewMicropost.java
+++ src/test/java/ru/my/mail/PublisherNewMicropost.java
@@ -16,28 +16,38 @@ public class PublisherNewMicropost {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://m.my.mail.ru/cgi-bin/login");
-        wd.findElement(By.name("Login")).click();
-        wd.findElement(By.name("Login")).sendKeys("\\undefined");
-        wd.findElement(By.name("Password")).click();
-        wd.findElement(By.name("Password")).sendKeys("\\undefined");
+        login("testqaqa", "Mytesting");
+    }
+
+    private void login(String username, String password) {
         wd.findElement(By.name("Login")).click();
         wd.findElement(By.name("Login")).clear();
-        wd.findElement(By.name("Login")).sendKeys("testqaqa");
+        wd.findElement(By.name("Login")).sendKeys(username);
         wd.findElement(By.name("Password")).click();
         wd.findElement(By.name("Password")).clear();
-        wd.findElement(By.name("Password")).sendKeys("Mytesting");
+        wd.findElement(By.name("Password")).sendKeys(password);
         wd.findElement(By.xpath("//div[@class='heightContent']/form/div[3]/input")).click();
     }
-    
+
     @Test
     public void testPublisherNewMicropost() {
-        wd.findElement(By.cssSelector("a.menu__item.m-publish")).click();
-        wd.findElement(By.name("arg_text")).click();
-        wd.findElement(By.name("arg_text")).clear();
-        wd.findElement(By.name("arg_text")).sendKeys("test");
+        goToPublisherPage();
+        fillPublisherForm("test");
+        submitPublisherCreation();
+    }
+
+    private void submitPublisherCreation() {
         wd.findElement(By.xpath("//div[@class='menu__list']//span[.='Отпр.']")).click();
     }
-    
+
+    private void fillPublisherForm(String name) {
+        wd.findElement(By.name("arg_text")).sendKeys(name);
+    }
+
+    private void goToPublisherPage() {
+        wd.findElement(By.cssSelector("a.menu__item.m-publish")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
